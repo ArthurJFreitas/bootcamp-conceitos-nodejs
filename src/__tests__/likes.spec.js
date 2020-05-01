a10c973154b6b337503c9561b086e310832cbf02
Arthur Junio <arthu_junior@hotmail.com>
2020-05-01 17:46:31 -0300

    solving challenge of repositories

--- src/__tests__/likes.spec.js
+++ src/__tests__/likes.spec.js
@@ -1,36 +1,36 @@
-const request = require("supertest");
-const app = require("../app");
+const request = require('supertest');
+const app = require('../app');
 
-describe("Likes", () => {
-  it("should be able to give a like to the repository", async () => {
+describe('Likes', () => {
+  it('should be able to give a like to the repository', async () => {
     const repository = await request(app)
-      .post("/repositories")
+      .post('/repositories')
       .send({
-        url: "https://github.com/Rocketseat/umbriel",
-        title: "Umbriel",
-        techs: ["Node", "Express", "TypeScript"]
+        url: 'https://github.com/Rocketseat/umbriel',
+        title: 'Umbriel',
+        techs: ['Node', 'Express', 'TypeScript'],
       });
 
     let response = await request(app).post(
-      `/repositories/${repository.body.id}/like`
+      `/repositories/${repository.body.id}/like`,
     );
 
     expect(response.body).toMatchObject({
-      likes: 1
+      likes: 1,
     });
 
     response = await request(app).post(
-      `/repositories/${repository.body.id}/like`
+      `/repositories/${repository.body.id}/like`,
     );
 
     expect(response.body).toMatchObject({
-      likes: 2
+      likes: 2,
     });
   });
 
-  it("should not be able to like a repository that does not exist", async () => {
+  it('should not be able to like a repository that does not exist', async () => {
     await request(app)
-      .post(`/repositories/123/like`)
+      .post('/repositories/123/like')
       .expect(400);
   });
 });
--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
-const express = require("express");
-const cors = require("cors");
+const express = require('express');
+const cors = require('cors');
 
-// const { uuid } = require("uuidv4");
+const { uuid } = require('uuidv4');
 
 const app = express();
 
@@ -10,24 +10,75 @@ app.use(cors());
 
 const repositories = [];
 
-app.get("/repositories", (request, response) => {
-  // TODO
+app.get('/repositories', (request, response) => response.json(repositories));
+
+app.post('/repositories', (request, response) => {
+  const { title, url, techs } = request.body;
+
+  const repository = {
+    id: uuid(), title, url, techs, likes: 0,
+  };
+
+  repositories.push(repository);
+  response.json(repository);
 });
 
-app.post("/repositories", (request, response) => {
-  // TODO
+app.put('/repositories/:id', (request, response) => {
+  const { id } = request.params;
+  const { title, url, techs } = request.body;
+
+  const RepoFilterIndex = repositories.findIndex((repo) => repo.id === id);
+
+  if (RepoFilterIndex < 0) {
+    return response.status(400).json({
+      error: 'Repository not found',
+    });
+  }
+
+  const repository = {
+    id: repositories[RepoFilterIndex].id,
+    title,
+    url,
+    techs,
+    likes: repositories[RepoFilterIndex].likes,
+  };
+
+  repositories[RepoFilterIndex] = repository;
+
+  return response.json(repository);
 });
 
-app.put("/repositories/:id", (request, response) => {
-  // TODO
+app.delete('/repositories/:id', (request, response) => {
+  const { id } = request.params;
+
+  const RepoFilterIndex = repositories.findIndex((repo) => repo.id === id);
+
+  if (RepoFilterIndex < 0) {
+    return response.status(400).json({
+      error: 'Repository not found',
+    });
+  }
+
+  repositories.splice(RepoFilterIndex, 1);
+
+  return response.status(204).send();
 });
 
-app.delete("/repositories/:id", (request, response) => {
-  // TODO
-});
+app.post('/repositories/:id/like', (request, response) => {
+  const { id } = request.params;
 
-app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+  const RepoFilterIndex = repositories.findIndex((repo) => repo.id === id);
+
+  if (RepoFilterIndex < 0) {
+    return response.status(400).json({
+      error: 'Repository not found',
+    });
+  }
+
+  repositories[RepoFilterIndex].likes++;
+
+
+  return response.json(repositories[RepoFilterIndex]);
 });
 
 module.exports = app;
